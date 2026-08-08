66adcb8adbd3d5499d10237645cb74bfc8900687
Charan <charanmr0717@gmail.com>
2026-08-08 23:26:40 +0530

    Added data types practice program

--- 01-basics/test.c
+++ 01-basics/test.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 int main(){
     int age = 17;
     printf("my age is %d", age);
@@ -6,12 +7,17 @@ int main(){
     float height = 5.8;
     printf("my height is %f", height);
 
-    
-    
-    
-    
-    
-    
-    
+    double pi = 3.141592653589;
+    printf("pi value is %lf\n", pi);
+
+    char grade = 'a';
+    printf("My grade is %c\n", grade);
+
+    char name[] = "Charan";
+    printf("My name is %s\n", name);
+
+    bool isstudent = true;
+    printf("Is student: %d\n", isstudent);
+
     return 0;
 }
